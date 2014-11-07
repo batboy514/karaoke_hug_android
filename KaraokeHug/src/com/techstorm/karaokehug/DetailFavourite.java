8fd29bd968a74b8b977a2e95f1671a4b1640fa7e
ngoc duc <dnduc456@gmail.com>
2014-11-07 19:35:11 +0700

    xong gan het

--- KaraokeHug/src/com/techstorm/karaokehug/DetailFavourite.java
+++ KaraokeHug/src/com/techstorm/karaokehug/DetailFavourite.java
@@ -14,13 +14,14 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class DisplayFavourite extends BaseAdapter{
+public class DetailFavourite extends BaseAdapter{
 	private Context mContext;
 	private ArrayList<String> id;
 	private ArrayList<String> name;
-	
-	private SQLiteDatabase dataBase;
-	 private DatabaseHelper mHelper;
+	private ArrayList<String> lyric;
+	private ArrayList<String> author;
+//	private SQLiteDatabase dataBase;
+//	 private DatabaseHelper mHelper;
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
@@ -33,7 +34,8 @@ public class DisplayFavourite extends BaseAdapter{
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("name", name.get(arg0));
 		map.put("id", id.get(arg0));
-		
+		map.put("lyric", lyric.get(arg0));
+		map.put("author", author.get(arg0));
 		return map;
 		
 		
@@ -55,50 +57,57 @@ public class DisplayFavourite extends BaseAdapter{
 		LayoutInflater layoutInflater;
 		if (arg1 == null) {
 			layoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			arg1 = layoutInflater.inflate(R.layout.listfavourite, null);
+			arg1 = layoutInflater.inflate(R.layout.detaillistfavourite, null);
 			mHolder = new Holder();
 			mHolder.txt_id = (TextView) arg1.findViewById(R.id.Txt_id);
 			mHolder.txt_name = (TextView) arg1.findViewById(R.id.Txt_name);
-			mHolder.btndel = (Button) arg1.findViewById(R.id.btndel);
-			arg1.setTag(mHolder);
+			mHolder.txt_lyric = (TextView) arg1.findViewById(R.id.TXT_LYRIC);
+			mHolder.txt_author = (TextView) arg1.findViewById(R.id.Txt_author);
+//			mHolder.btndel = (Button) arg1.findViewById(R.id.btndel);
+//			arg1.setTag(mHolder);
 		} else {
 			mHolder = (Holder) arg1.getTag();
 		}
 		mHolder.txt_name.setText(name.get(arg0));
 		mHolder.txt_id.setText(id.get(arg0));
-		mHolder.btndel.setTag(id.get(arg0));
-		mHolder.btndel.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				String id = (String)v.getTag();
-				 dataBase = mHelper.getWritableDatabase();
-					mHelper.openDatabase();
-					
-				int value = Integer.parseInt(id);
-				String strSQL = "UPDATE ZSONG SET favourite = 0 WHERE ZROWID = "+ value + ";";
-				dataBase.execSQL(strSQL);
-				FavouriteActivity favActivity =  (FavouriteActivity)mContext;
-				favActivity.displayDataALL();
-			}
-		});
+		mHolder.txt_lyric.setText(lyric.get(arg0));
+		mHolder.txt_author.setText(author.get(arg0));
+//		mHolder.btndel.setTag(id.get(arg0));
+//		mHolder.btndel.setOnClickListener(new OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+//				String id = (String)v.getTag();
+//				 dataBase = mHelper.getWritableDatabase();
+//					mHelper.openDatabase();
+//					
+//				int value = Integer.parseInt(id);
+//				String strSQL = "UPDATE ZSONG SET favourite = 0 WHERE ZROWID = "+ value + ";";
+//				dataBase.execSQL(strSQL);
+//				FavouriteActivity favActivity =  (FavouriteActivity)mContext;
+//				favActivity.displayDataALL();
+//			}
+//		});
 		return arg1;
 	}
 	
 	private Context context;
-	public DisplayFavourite(Context c,ArrayList<String> id,ArrayList<String> name) {
+	public DetailFavourite(Context c,ArrayList<String> id,ArrayList<String> name,ArrayList<String> lyric,ArrayList<String> author) {
 		this.mContext = c;
 		this.id = id;
 		this.name = name;
-		mHelper = new DatabaseHelper(c);
+//		mHelper = new DatabaseHelper(c);
+		this.lyric = lyric;
+		this.author = author;
 	}
 	
 
 	public class Holder {
 		TextView txt_id;
 		TextView txt_name;
-		Button btndel;
-		
+//		Button btndel;
+		TextView txt_lyric;
+		TextView txt_author;
 	}
 }
--- KaraokeHug/src/com/techstorm/karaokehug/FavouriteActivity.java
+++ KaraokeHug/src/com/techstorm/karaokehug/FavouriteActivity.java
@@ -1,36 +1,58 @@
 package com.techstorm.karaokehug;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 public class FavouriteActivity extends Activity {
 	 private DatabaseHelper mHelper;
 	 private SQLiteDatabase dataBase;
-	 ListView userlistsearch;
+	 ListView userlistfavourite;
 	 private ArrayList<String> user_name = new ArrayList<String>();
 		private ArrayList<String> user_id = new ArrayList<String>();
-	
+		
+
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.favourite);
-        userlistsearch = (ListView) findViewById(R.id.userlistfavourite);
+        userlistfavourite = (ListView) findViewById(R.id.userlistfavourite);
         mHelper = new DatabaseHelper(this);
         dataBase = mHelper.getWritableDatabase();
     	mHelper.openDatabase();
     	displayDataALL();
-        
+        userlistfavourite.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				 userlistfavourite = (ListView) findViewById(R.id.userlistfavourite);
+				Map<String, Object> map = (Map<String, Object>)userlistfavourite.getItemAtPosition(arg2);
+				// TODO Auto-generated method stub
+				 Intent intent = new Intent();
+				 
+				 intent.putExtra("name", (String)map.get("name"));
+				 intent.putExtra("id", (String)map.get("id"));
+      intent.setClass(getApplicationContext(), ArrowsActivity.class);
+		            startActivity(intent);			
+			}
+		});
         
         
         
 	}
 	
 	
-	private void displayDataALL( ) {
+	public void displayDataALL( ) {
 		dataBase = mHelper.getWritableDatabase();
 		mHelper.openDatabase();
 		String query = "SELECT ZSNAME,ZROWID FROM ZSONG where favourite = 1";
@@ -44,11 +66,10 @@ public class FavouriteActivity extends Activity {
 				do {
 					user_id.add(mCursor.getString(mCursor.getColumnIndex("ZROWID")));
 					user_name.add(mCursor.getString(mCursor.getColumnIndex("ZSNAME")));
-					
 				} while (mCursor.moveToNext());
 			}
 			DisplayFavourite disadpt2 = new DisplayFavourite(FavouriteActivity.this,user_id, user_name);
-					userlistsearch.setAdapter(disadpt2);
+					userlistfavourite.setAdapter(disadpt2);
 					mCursor.close();
 				
 	}
